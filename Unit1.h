68297a36fec0d2f999aef8240e564bc3c4803a78
243271 <56536556+243271@users.noreply.github.com>
2019-11-03 22:01:35 +0100

    Add files via upload

--- Unit1.h
+++ Unit1.h
@@ -8,39 +8,68 @@
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
 #include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *pole3;
         TImage *pole2;
         TImage *pole1;
-        TImage *pole3;
-        TImage *pole4;
-        TImage *pole5;
-        TImage *pole7;
-        TImage *pole6;
-        TImage *pole8;
-        TImage *pole9;
-        TImage *pole10;
-        TImage *pole12;
-        TImage *pole11;
-        TImage *pole13;
-        TImage *pole14;
-        TImage *pole15;
-        TImage *pole17;
-        TImage *pole16;
-        TImage *pole18;
-        TImage *pole19;
-        TImage *pole20;
+        TLabel *Label1;
+        TImage *tura;
+        TButton *Button1;
+        TLabel *wyg_o;
+        TLabel *wyg_x;
+        TImage *Image1;
+        TImage *Image2;
+        TStaticText *StaticText1;
+        TImage *pole25;
+        TImage *pole24;
+        TImage *pole23;
         TImage *pole22;
         TImage *pole21;
-        TImage *pole23;
-        TImage *pole24;
-        TImage *pole25;
-        TImage *tura;
-        TLabel *Label1;
-        TButton *Button1;
+        TImage *pole20;
+        TImage *pole19;
+        TImage *pole18;
+        TImage *pole17;
+        TImage *pole16;
+        TImage *pole15;
+        TImage *pole10;
+        TImage *pole5;
+        TImage *pole14;
+        TImage *pole9;
+        TImage *pole4;
+        TImage *pole6;
+        TImage *pole7;
+        TImage *pole8;
+        TImage *pole11;
+        TImage *pole12;
+        TImage *pole13;
         void __fastcall FormCreate(TObject *Sender);
+        void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
+        void __fastcall FormMouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole1MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole2MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole3MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole4MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole5MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole6MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole7MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole8MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole9MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall Button1MouseDown(TObject *Sender,
+          TMouseButton Button, TShiftState Shift, int X, int Y);
         void __fastcall pole1Click(TObject *Sender);
         void __fastcall pole2Click(TObject *Sender);
         void __fastcall pole3Click(TObject *Sender);
@@ -62,10 +91,42 @@ __published:	// IDE-managed Components
         void __fastcall pole19Click(TObject *Sender);
         void __fastcall pole20Click(TObject *Sender);
         void __fastcall pole21Click(TObject *Sender);
-        void __fastcall pole23Click(TObject *Sender);
         void __fastcall pole22Click(TObject *Sender);
+        void __fastcall pole23Click(TObject *Sender);
         void __fastcall pole24Click(TObject *Sender);
         void __fastcall pole25Click(TObject *Sender);
+        void __fastcall pole10MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole11MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole12MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole13MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole14MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole15MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole16MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole17MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole18MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole19MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole20MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole21MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole22MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole23MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole24MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
+        void __fastcall pole25MouseMove(TObject *Sender, TShiftState Shift,
+          int X, int Y);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
